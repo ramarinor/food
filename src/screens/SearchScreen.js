5cdd6ecae6e392324b94097a58c9ad1c5b3b1e62
ramarinor <rinor.rama95@gmail.com>
2022-05-12 13:17:02 +0200

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import Searchbar from '../components/Searchbar';
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Searchbar />
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default SearchScreen;
